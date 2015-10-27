556951f6672b3d3a3c9d8b5d2e3149a3f4f3695a
kojirou <kojirouhtc@gmail.com>
2015-10-27 23:06:41 +0800

    Initial Commit

---
+++ YunBaDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YunBaDemo
+//
+//  Created by 王宇 on 15/10/27.
+//  Copyright © 2015年 emwcn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
